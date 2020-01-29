3540bf02a436d7304b72802db105669615eccd20
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-01-29 23:45:56 +0300

    Update critter.js

--- js/critter.js
+++ js/critter.js
@@ -32,7 +32,7 @@ class Critter extends Obj {
 
         if (this.eatenTimer < 50) {
             for (var i = 0; i < objs.length; i++) {
-                if (objs[i] && objs[i] instanceOf Food && this.eatenTimer < 50) {
+                if (objs[i] && (objs[i] instanceof Food) && this.eatenTimer < 50) {
                     var distance = p5.Vector.dist(this.position, objs[i].getPosition());
                     if ((this != objs[i]) && (distance < this.size + objs[i].getSize())) {
                         this.eatenTimer += objs[i].getSize() * 1;
