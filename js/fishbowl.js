fd623e41ef7a88b840dbbe56eed75d29a3f423a2
leshchenko1979 <leshchenko@gmail.com>
2020-02-21 18:40:54 +0100

    Bugfix

--- js/fishbowl.js
+++ js/fishbowl.js
@@ -46,7 +46,7 @@ function mouseClicked() {
 */
 
 function setup() {
-    var canvas = createCanvas(windowWidth - 220, windowHeight - 25);
+    var canvas = createCanvas(windowWidth, windowHeight - 30);
     canvas.parent('canvas');
     background("gray");
     colorMode(HSB, 100);
